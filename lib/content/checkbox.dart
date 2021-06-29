15d991b3c97899a5c4b575e6f999ebb8ed846625
Amine <kurosoul.ae@gmail.com>
2021-06-29 20:27:11 +0100

    add blank method for creating blank models

--- lib/content/checkbox.dart
+++ lib/content/checkbox.dart
@@ -1,151 +1,148 @@
 import 'package:flutter/material.dart';
 
-
 class CheckboxWidget extends StatefulWidget {
-	CheckboxWidgetModel model;
+  final CheckboxWidgetModel model;
 
-	CheckboxWidget({Key? key, required this.model}): super(key: key);
+  CheckboxWidget({Key? key, required this.model}) : super(key: key);
 
-	@override
-	_CheckboxWidgetState createState() => _CheckboxWidgetState();
+  @override
+  _CheckboxWidgetState createState() => _CheckboxWidgetState();
 }
 
 class _CheckboxWidgetState extends State<CheckboxWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          ...buildLines(),
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              widget.model.lines.add(
+                CheckLine.fromMap({'checked': false, 'content': ''}),
+              );
 
+              setState(() => widget.model.save());
+            },
+          ),
+        ],
+      ),
+    );
+  }
 
-	@override
-	Widget build(BuildContext context) {
-		return Container(
-			child: Column(
-				children: [
-					...buildLines(),
-
-					IconButton(
-						icon: Icon(Icons.add),
-						onPressed: () {
-							widget.model.lines.add(CheckLine.fromMap({
-								'checked': false,
-								'content': ''
-							}));
-
-							setState(() {
-								widget.model.save();
-							});
-						}
-					),
-				],
-			),
-		);
-	}
-
-	List<Widget> buildLines() {
-		List<Widget> lines = [];
-		for(int index = 0; index < widget.model.lines.length; index++) {
-			lines.add(Row(
-				children: [
-					Checkbox(
-						value: widget.model.lines[index].checked,
-						onChanged: (bool? value) {
-							setState(() {
-								widget.model.lines[index].checked = value ?? false;
-								widget.model.save();
-							});
-						},
-					),
-					Expanded(
-						child: TextFormField(
-							controller: widget.model.lines[index].content,
-						),
-					),
-					IconButton(
-						onPressed: () {
-							setState(() {
-								widget.model.lines.removeAt(index);
-								widget.model.save();
-							});
-						},
-						icon: Icon(Icons.delete),
-					),
-				],
-			));
-		}
-		return lines;
-	}
+  List<Widget> buildLines() {
+    List<Widget> lines = [];
+    for (int index = 0; index < widget.model.lines.length; index++) {
+      lines.add(Row(
+        children: [
+          Checkbox(
+            value: widget.model.lines[index].checked,
+            onChanged: (bool? value) {
+              setState(() {
+                widget.model.lines[index].checked = value ?? false;
+                widget.model.save();
+              });
+            },
+          ),
+          Expanded(
+            child: TextFormField(
+              controller: widget.model.lines[index].content,
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              setState(() {
+                widget.model.lines.removeAt(index);
+                widget.model.save();
+              });
+            },
+            icon: Icon(Icons.delete),
+          ),
+        ],
+      ));
+    }
+    return lines;
+  }
 }
 
 class CheckLine with ChangeNotifier {
-	bool checked;
-	TextEditingController content;
+  bool checked;
+  TextEditingController content;
 
-	CheckLine({
-		required this.checked,
-		required this.content,
-	});
+  CheckLine({
+    required this.checked,
+    required this.content,
+  });
 
-	Map<String, dynamic> toJson() {
-		return {
-			'checked': this.checked,
-			'content': this.content.text,
-		};
-	}
+  Map<String, dynamic> toJson() {
+    return {
+      'checked': this.checked,
+      'content': this.content.text,
+    };
+  }
 
+  static CheckLine fromMap(Map<String, dynamic> map) {
+    return CheckLine(
+      checked: map['checked'] as bool,
+      content: TextEditingController(text: map['content'] as String),
+    );
+  }
 
-	static CheckLine fromMap(Map<String, dynamic> map) {
-		return CheckLine(
-			checked: map['checked'] as bool,
-			content: TextEditingController(text: map['content'] as String),
-		);
-	}
-
-	void save() {
-		notifyListeners();
-	}
+  void save() {
+    notifyListeners();
+  }
 }
 
-
 class CheckboxWidgetModel with ChangeNotifier {
-	int position;
-	List<CheckLine> lines = [];
+  int position;
+  List<CheckLine> lines = [];
 
-	CheckboxWidgetModel({
-		required this.lines,
-		required this.position,
-	});
+  CheckboxWidgetModel({
+    required this.lines,
+    required this.position,
+  });
 
-	Map<String, dynamic> toJson() {
-		List<Map<String, dynamic>> content = [];
-		lines.forEach((element) {
-			content.add(element.toJson());
-		});
+  Map<String, dynamic> toJson() {
+    List<Map<String, dynamic>> content = [];
+    lines.forEach((element) {
+      content.add(element.toJson());
+    });
 
-		return {
-			'name': 'CheckboxWidget',
-			'lines': content,
-			'position': this.position,
-		};
-	}
+    return {
+      'name': 'CheckboxWidget',
+      'lines': content,
+      'position': this.position,
+    };
+  }
 
-	static CheckboxWidgetModel fromMap(Map<String, dynamic> map) {
-		List<CheckLine> content = [];
-		for(var element in map['lines']) {
-			content.add(CheckLine.fromMap(element));
-		}
+  static CheckboxWidgetModel fromMap(Map<String, dynamic> map) {
+    List<CheckLine> content = [];
+    for (var element in map['lines']) {
+      content.add(CheckLine.fromMap(element));
+    }
 
-		return CheckboxWidgetModel(
-			lines: content,
-			position: map['position'] as int,
-		);
-	}
+    return CheckboxWidgetModel(
+      lines: content,
+      position: map['position'] as int,
+    );
+  }
 
-	void deleteLine(index) {
-		lines.removeAt(index);
-	}
+  void deleteLine(index) {
+    lines.removeAt(index);
+  }
 
-	void save() {
-		for(var element in lines) {
-			element.save();
-		}
+  void save() {
+    for (var element in lines) {
+      element.save();
+    }
 
-		notifyListeners();
-	}
+    notifyListeners();
+  }
+
+  static CheckboxWidgetModel blank() {
+    List<CheckLine> content = [];
+
+    return CheckboxWidgetModel(lines: content, position: 1);
+  }
 }
--- lib/content/text.dart
+++ lib/content/text.dart
@@ -1,53 +1,60 @@
 import 'package:flutter/material.dart';
 
-
 class TextWidget extends StatefulWidget {
-	TextWidgetModel model;
+  final TextWidgetModel model;
 
-	TextWidget({Key? key, required this.model}): super(key: key);
+  TextWidget({Key? key, required this.model}) : super(key: key);
 
-	@override
-	_TextWidgetState createState() => _TextWidgetState();
+  @override
+  _TextWidgetState createState() => _TextWidgetState();
+
 }
 
 class _TextWidgetState extends State<TextWidget> {
-
-		@override
-	Widget build(BuildContext context) {
-		return Container(
-			child: TextFormField(
-				controller: widget.model.content,
-			),
-		);
-	}
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TextFormField(
+        maxLines: null,
+        controller: widget.model.content,
+        keyboardType: TextInputType.text,
+      ),
+    );
+  }
 }
 
-
 class TextWidgetModel with ChangeNotifier {
-	int position;
-	TextEditingController content;
+  int position;
+  TextEditingController content;
 
-	TextWidgetModel({
-		required this.content,
-		required this.position,
-	});
+  TextWidgetModel({
+    required this.content,
+    required this.position,
+  });
 
-	Map<String, dynamic> toJson() {
-		return {
-			'name': 'TextWidget',
-			'position': this.position,
-			'content': this.content.text,
-		};
-	}
+  Map<String, dynamic> toJson() {
+    return {
+      'name': 'TextWidget',
+      'position': this.position,
+      'content': this.content.text,
+    };
+  }
 
-	static TextWidgetModel fromMap(Map<String, dynamic> map) {
-		return TextWidgetModel(
-			position: map['position'] as int,
-			content: TextEditingController(text: map['content'] as String),
-		);
-	}
+  static TextWidgetModel fromMap(Map<String, dynamic> map) {
+    return TextWidgetModel(
+      position: map['position'] as int,
+      content: TextEditingController(text: map['content'] as String),
+    );
+  }
 
-	void save() {
-    	notifyListeners();
-    }
+  static TextWidgetModel blank() {
+    return TextWidgetModel(
+      position: 1,
+      content: TextEditingController(text: ''),
+    );
+  }
+
+  void save() {
+    notifyListeners();
+  }
 }
